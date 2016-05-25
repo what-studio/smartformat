d14aa9671c7443de42ce4341182455ceae76baf0
Heungsub Lee <sub@subl.ee>
2016-05-25 18:25:05 +0900

    Make setup.py

---
+++ setup.py
@@ -0,0 +1,37 @@
+import os
+
+from setuptools import find_packages, setup
+
+
+# Include __about__.py.
+__dir__ = os.path.dirname(__file__)
+about = {}
+with open(os.path.join(__dir__, 'smartformat', '__about__.py')) as f:
+    exec(f.read(), about)
+
+
+setup(
+    name='smartformat',
+    version=about['__version__'],
+    license=about['__license__'],
+    author=about['__author__'],
+    maintainer=about['__maintainer__'],
+    maintainer_email=about['__maintainer_email__'],
+    url='https://github.com/what-studio/smartformat',
+    description='Python implementation of SmartFormat.NET',
+    platforms='any',
+    packages=find_packages(),
+    zip_safe=False,
+    classifiers=['Development Status :: 4 - Beta',
+                 'Intended Audience :: Developers',
+                 'License :: OSI Approved :: BSD License',
+                 'Operating System :: OS Independent',
+                 'Programming Language :: Python',
+                 'Programming Language :: Python :: 2',
+                 'Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: 3.4',
+                 'Programming Language :: Python :: Implementation :: CPython',
+                 'Programming Language :: Python :: Implementation :: PyPy',
+                 'Topic :: Software Development'],
+    install_requires=['babel', 'six'],
+)
---
+++ smartformat/__about__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+   smartformat.__about__
+   ~~~~~~~~~~~~~~~~~~~~~
+"""
+__version__ = '0.0.1'
+__license__ = 'BSD'
+__author__ = 'What! Studio'
+__maintainer__ = 'Heungsub Lee'
+__maintainer_email__ = 'sub@nexon.co.kr'
