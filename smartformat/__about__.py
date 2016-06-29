2dfe949c95eb00cf8ed891e316a5c6c4260f6dcd
Heungsub Lee <sub@subl.ee>
2016-06-29 14:35:51 +0900

    Bump version: 0.0.2

--- smartformat/__about__.py
+++ smartformat/__about__.py
@@ -3,7 +3,7 @@
    smartformat.__about__
    ~~~~~~~~~~~~~~~~~~~~~
 """
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 __license__ = 'BSD'
 __author__ = 'What! Studio'
 __maintainer__ = 'Heungsub Lee'
